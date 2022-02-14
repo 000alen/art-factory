a60b7cbfc575d1bc3474b049e28a06c10a61f9ff
Alen Rubilar <lclc.alen@gmail.com>
2022-02-13 23:16:15 -0300

    Add ui-react-starter

--- uxp/webpack.config.js
+++ uxp/webpack.config.js
@@ -1,37 +1,56 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
-  resolve: {
-    extensions: ['.mjs', '.js'],
-  },
+  entry: "./src/index.jsx",
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'index.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    //libraryTarget: "commonjs2"
   },
+  //   devtool: "cheap-eval-source-map", // won't work on XD due to lack of eval
+  devtool: "eval-cheap-source-map", // won't work on XD due to lack of eval
   externals: {
-    uxp: 'commonjs2 uxp',
+    uxp: "commonjs2 uxp",
+    photoshop: "commonjs2 photoshop",
+    os: "commonjs2 os",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
-          presets: ['@babel/preset-env'],
+          plugins: [
+            "@babel/transform-react-jsx",
+            "@babel/proposal-object-rest-spread",
+            "@babel/plugin-syntax-class-properties",
+          ],
         },
       },
       {
+        test: /\.png$/,
+        exclude: /node_modules/,
+        loader: "file-loader",
+      },
+      {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   plugins: [
+    // new CleanWebpackPlugin(),
+    // new CopyPlugin(['plugin'], {
+    //     copyUnmodified: true
+    // })
     new CopyPlugin({
-      patterns: ['plugin'],
+      patterns: [{ from: "plugin", to: "." }],
     }),
   ],
 };
