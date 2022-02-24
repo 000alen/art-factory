3d4632e9c2f71074a55d39f889617431accb356b
Sebastián Torrealba <sebastian.torrealba@usm.cl>
2022-02-24 17:04:09 -0300

    Added some Task Items

--- uxp-host/src/components/Panel1155.jsx
+++ uxp-host/src/components/Panel1155.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { TaskItem } from "./TaskItem";
+import { Flex } from "@adobe/react-spectrum";
+
+
 
 export function Panel1155({
   contract,
@@ -7,5 +11,117 @@ export function Panel1155({
   addOutput,
   dialogContext,
 }) {
-  return <div>Panel1155</div>;
+  return (
+    <>
+      <Flex direction="column" gap="size-100">
+        <TaskItem
+          task="Balance of"
+          fields={[
+            {
+              key: "address",
+              type: "address",
+              label: "Address",
+            },
+            {
+              key: "id",
+              type: "int",
+              label: "Id"
+            }
+          ]}
+        />
+
+        <TaskItem
+          task="Token URI"
+          fields={[
+            {
+              key: "input",
+              type: "int",
+              label: "Input"
+            }
+          ]}
+        />
+
+        <TaskItem
+          task="URI"
+          fields={[
+            {
+              key: "id",
+              type: "int",
+              label: "Id"
+            }
+          ]}
+        />
+      </Flex>
+
+      <Flex direction="column" gap="size-100">
+        <TaskItem
+            task="Burn"
+            fields={[
+              {
+                key: "id",
+                type: "int",
+                label: "Id"
+              },
+              {
+                key: "amount",
+                type: "int",
+                label: "Amount"
+              }
+            ]}
+          />
+
+          <TaskItem
+            task="Mint"
+            fields={[
+              {
+                key: "to",
+                type: "address",
+                label: "To"
+              },
+              {
+                key: "id",
+                type: "int",
+                label: "Id"
+              },
+              {
+                key: "amount",
+                type: "int",
+                label: "Amount"
+              }
+            ]}
+          />
+
+          <TaskItem
+            task="Set URI"
+            fields={[
+              {
+                key: "id",
+                type: "int",
+                label: "Id"
+              },
+              {
+                key: "uri",
+                type: "string",
+                label: "URI"
+              }
+            ]}
+          />
+      </Flex>
+    </>
+  );
 }
+
+/*
+balanceOf       <
+balanceOfBatch  Missing (Array)
+tokenURI        <
+uri             <
+
+burn            <
+burnBatch       Missing (Array)
+burnFromMint    Missing (Array)
+Mint            <
+MintBatch       Missing (Array)
+safeBatchTransferFRom Missing (Array)
+setURI
+*/
