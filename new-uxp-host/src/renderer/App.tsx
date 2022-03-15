5fb87f9d8e59d032dc22573a5471817c2c7cfd2d
Alen Rubilar <lclc.alen@gmail.com>
2022-03-15 18:45:48 -0300

    Partial<Refactor>

---
+++ new-uxp-host/src/renderer/App.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./index.css";
+
+function render() {
+  ReactDOM.render(<h2>Hello from React!</h2>, document.body);
+}
+
+render();
