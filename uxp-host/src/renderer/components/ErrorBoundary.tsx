7897b7b3b58425c2ac3c0ec26f263fb8a9066c32
Sebastián Torrealba <sebastian.torrealba@usm.cl>
2022-04-17 21:09:02 -0400

    Error page, todo: create navigate with home button

--- uxp-host/src/renderer/components/ErrorBoundary.tsx
+++ uxp-host/src/renderer/components/ErrorBoundary.tsx
@@ -1,7 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-import { Heading, Text } from "@adobe/react-spectrum";
+import { Heading, Text, View, Flex, Divider, Button } from "@adobe/react-spectrum";
+import Bug from "@spectrum-icons/workflow/Bug";
+import Home from "@spectrum-icons/workflow/Home";
+
+
 
 interface ErrorBoundaryProps {}
 
@@ -27,13 +30,38 @@ export class ErrorBoundary extends React.Component<
   render() {
     if (this.state.hasError) {
       return (
-        <>
-          <Heading>Something went wrong :(</Heading>
-          <Text>{this.state.error.message}</Text>
-          <Link to="/" onClick={() => this.setState({ hasError: false })}>
-            Go home
-          </Link>
-        </>
+        <Flex UNSAFE_className="h-screen">
+          <View margin="auto">
+
+            <Flex alignItems="center">
+              <Bug margin="size-100"/>
+              <Heading level={1}> Something went wrong...</Heading>
+            </Flex>
+
+            <View
+              borderWidth="thin"
+              borderColor="dark"
+              borderRadius="medium"
+              padding="size-250"
+              width="size-6000"
+            >
+              <Heading level={3} >Error log</Heading>
+              <Divider marginY="size-100"/>
+              <Text>{this.state.error.message}</Text>
+            </View>
+
+            <Flex justifyContent="right">
+              <Button variant="cta" marginY="size-100" onPress={() => {
+                this.setState({ hasError: false })} 
+              }>
+                <Flex alignItems="center">
+                  <Home margin="size-100" />
+                  <Text> Go home!</Text>
+                </Flex>
+              </Button>
+            </Flex>
+          </View>
+        </Flex>
       );
     }
 
